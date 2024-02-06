976c6173ff48fbf96b8278f9cbe0688d13eafe19
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2024-02-06 21:12:26 +0000

    fixed

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,13 +2,13 @@ export default function createReportObject(employeesList) {
   const allEmployees = {};
 
   for (const [departmentName, employees] of Object.entries(employeesList)) {
-  allEmployees[departmentName] = [...employees];
-}
+    allEmployees[departmentName] = [...employees];
+  }
 
   return {
   allEmployees,
-  getNumberOfDepartments() {
-  return Object.keys(employeesList).length;
-},
-};
+    getNumberOfDepartments() {
+      return Object.keys(employeesList).length;
+    },
+  };
 }
