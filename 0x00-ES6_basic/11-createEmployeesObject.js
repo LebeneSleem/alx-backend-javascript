526586140d01e682a5c1780beba2051237166232
Lebene Gbebleou-Sleem <ChantelleSleem@gmail.com>
2024-02-06 21:10:31 +0000

    lint error fixed

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,7 +1,7 @@
 export default function createEmployeesObject(departmentName, employees) {
   const employeesObject = {
-  [departmentName]: employees,
-};
+    [departmentName]: employees,
+  };
 
   return employeesObject;
 }
